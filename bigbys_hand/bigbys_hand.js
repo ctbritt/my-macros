f7f37f71691a55a3e759ae8941b5ae28d3b6562e
Chris Allbritton <callbritton@mac.com>
2023-03-31 17:03:30 +0000

    update bigby’s hand

--- bigbys_hand/bigbys_hand.js
+++ bigbys_hand/bigbys_hand.js
@@ -2,130 +2,106 @@ const actorD = game.actors.get(args[0].actor._id);
 const tokenD = canvas.tokens.get(args[0].tokenId);
 const level = args[0].spellLevel;
 const summonType = "Bigby's Hand";
-const summonerDc = actorD.data.data.attributes.spelldc;
+const summonerDc = actorD.system.attributes.spelldc;
 const summonerAttack = summonerDc - 8;
 const summonerMod = getProperty(
   tokenD.actor,
-  `data.data.abilities.${getProperty(
-    tokenD.actor,
-    "data.data.attributes.spellcasting"
+  `system.abilities.${getProperty(
+  tokenD.actor,
+  "system.attributes.spellcasting"
   )}.mod`
 );
 
-let fistScale = "";
-let graspScale = "";
+const fistScale = level - 5 > 0 ? ` + ${(level - 5) * 2}d8[force]` : "";
+const graspScale = level - 5 > 0 ? ` + ${(level - 5) * 2}d6[bludgeoning] + ${summonerMod}` : "";
 
-if (level - 5 > 0) {
-  fistScale = ` + ${(level - 5) * 2}d8[force]`;
-}
-if (level - 5 > 0) {
-  graspScale = ` + ${(level - 5) * 2}d6[bludgeoning]`;
-}
-
-let choice = await new Promise((resolve) => {
+async function chooseColor() {
+  const colorDialog = new Promise((resolve) => {
   new Dialog({
     title: "Choose your color:",
     buttons: {
-      one: {
-        label:
-          '<img src="modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Blue_Thumb.webp" width="40" height="40" style="border:0px"><br>Blue',
-        callback: () => {
-          resolve({
-            token: {
-              img: "modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Blue_400x400.webm",
-            },
-          });
-        },
-      },
-      two: {
-        label:
-          '<img src="modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Green_Thumb.webp" width="40" height="40" style="border:0px"><br>Green',
-        callback: () => {
-          resolve({
-            token: {
-              img: "modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Green_400x400.webm",
-            },
-          });
-        },
-      },
-      three: {
-        label:
-          '<img src="modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Purple_Thumb.webp" width="40" height="40" style="border:0px"><br>Purple',
-        callback: () => {
-          resolve({
-            token: {
-              img: "modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Purple_400x400.webm",
-            },
-          });
-        },
-      },
-      fourth: {
-        label:
-          '<img src="modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Red_Thumb.webp" width="40" height="40" style="border:0px"><br>Red',
-        callback: () => {
-          resolve({
-            token: {
-              img: "modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Red_400x400.webm",
-            },
-          });
-        },
-      },
+    blue: createColorButton("Blue", resolve),
+    green: createColorButton("Green", resolve),
+    purple: createColorButton("Purple", resolve),
+    red: createColorButton("Red", resolve),
     },
   }).render(true);
-});
+  });
+
+  return await colorDialog;
+}
+
+function createColorButton(color, resolve) {
+  const label =
+  `<img src="modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_${color}_Thumb.webp" width="40" height="40" style="border:0px"><br>${color}`;
+
+  return {
+  label,
+  callback: () => {
+    resolve({
+    token: {
+      img: `modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_${color}_400x400.webm`,
+    },
+    });
+  },
+  };
+}
+
+let choice = await chooseColor();
 
 let updates = {
   token: {
-    alpha: 0,
-    name: `${summonType} of ${actorD.name}`,
+  alpha: 0,
+  name: `${summonType} of ${actorD.name}`,
   },
   actor: {
-    name: `${summonType} of ${actorD.name}`,
-    "data.attributes.hp": {
-      value: actorD.data.data.attributes.hp.max,
-      max: actorD.data.data.attributes.hp.max,
-    },
+  name: `${summonType} of ${actorD.name}`,
+  "data.attributes.hp": {
+    value: actorD.system.attributes.hp.max,
+    max: actorD.system.attributes.hp.max,
+  },
   },
   embedded: {
-    Item: {
-      "Clenched Fist": {
-        "data.attackBonus": `- @mod - @prof + ${summonerAttack}`,
-        "data.damage.parts": [[`4d8[force] ${fistScale}`, "force"]],
-      },
-      "Grasping Hand (Crush)": {
-        "data.damage.parts": [
-          [`2d6[bludgeoning] + ${summonerMod} ${graspScale}`, "bludgeoning"],
-        ],
-      },
+  Item: {
+    "Clenched Fist": {
+    "data.attackBonus": `- @mod - @prof + ${summonerAttack}`,
+    "data.damage.parts": [[`4d8[force] ${fistScale}`, "force"]],
     },
+    "Grasping Hand (Crush)": {
+    "data.damage.parts": [
+      [`2d6[bludgeoning] ${graspScale}`, "bludgeoning"],
+    ],
+    },
+  },
   },
 };
 
+// ... rest of the code
 function greetings(templateData, summonedToken) {
   ChatMessage.create({ content: `Bigby's hand appears.` });
 
   let colorChoice = summonedToken.data.img;
 
   if (colorChoice.includes("Blue")) {
-    return ChatMessage.create({
-      speaker: { alias: "Bigby's Hand" },
-      content: `<img src="modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Blue_Thumb.webp" width="30" height="30" style="border:0px"> <i>Gives ${actorD.name} a thumbs up.</i> 👍`,
-    });
+  return ChatMessage.create({
+  speaker: { alias: "Bigby's Hand" },
+  content: `<img src="modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Blue_Thumb.webp" width="30" height="30" style="border:0px"> <i>Gives ${actorD.name} a thumbs up.</i> 👍`,
+  });
   } else if (colorChoice.includes("Green")) {
-    return ChatMessage.create({
-      speaker: { alias: "Bigby's Hand" },
-      content: `<img src="modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Green_Thumb.webp" width="30" height="30" style="border:0px"> <i>Throws a peace sign at ${actorD.name}.</i> ✌️`,
-    });
+  return ChatMessage.create({
+  speaker: { alias: "Bigby's Hand" },
+  content: `<img src="modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Green_Thumb.webp" width="30" height="30" style="border:0px"> <i>Throws a peace sign at ${actorD.name}.</i> ✌️`,
+  });
   } else if (colorChoice.includes("Purple")) {
-    return ChatMessage.create({
-      speaker: { alias: "Bigby's Hand" },
-      content: `<img src="modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Purple_Thumb.webp" width="30" height="30" style="border:0px"> <i>Waves at ${actorD.name}.</i> 👋`,
-    });
+  return ChatMessage.create({
+  speaker: { alias: "Bigby's Hand" },
+  content: `<img src="modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Purple_Thumb.webp" width="30" height="30" style="border:0px"> <i>Waves at ${actorD.name}.</i> 👋`,
+  });
   } else if (colorChoice.includes("Red")) {
-    return ChatMessage.create({
-      speaker: { alias: "Bigby's Hand" },
-      content: `<img src="modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Red_Thumb.webp" width="30" height="30" style="border:0px"> <i>Waits for ${actorD.name} to give a high five.</i> 🖐️️`,
-    });
+  return ChatMessage.create({
+  speaker: { alias: "Bigby's Hand" },
+  content: `<img src="modules/jb2a_patreon/Library/5th_Level/Arcane_Hand/ArcaneHand_Human_01_Idle_Red_Thumb.webp" width="30" height="30" style="border:0px"> <i>Waits for ${actorD.name} to give a high five.</i> 🖐️️`,
+  });
   }
 }
 
@@ -136,31 +112,31 @@ async function myEffectFunction(template, update) {
   let colorChoice = update.token.img;
 
   if (colorChoice.includes("Blue")) {
-    effect =
-      "modules/jb2a_patreon/Library/Generic/Explosion/Explosion_01_Blue_400x400.webm";
+  effect =
+  "modules/jb2a_patreon/Library/Generic/Explosion/Explosion_01_Blue_400x400.webm";
   } else if (colorChoice.includes("Green")) {
-    effect =
-      "modules/jb2a_patreon/Library/Generic/Explosion/Explosion_01_Green_400x400.webm";
+  effect =
+  "modules/jb2a_patreon/Library/Generic/Explosion/Explosion_01_Green_400x400.webm";
   } else if (colorChoice.includes("Purple")) {
-    effect =
-      "modules/jb2a_patreon/Library/Generic/Explosion/Explosion_01_Purple_400x400.webm";
+  effect =
+  "modules/jb2a_patreon/Library/Generic/Explosion/Explosion_01_Purple_400x400.webm";
   } else if (colorChoice.includes("Red")) {
-    effect =
-      "modules/jb2a_patreon/Library/Generic/Explosion/Explosion_04_Dark_Red_400x400.webm";
+  effect =
+  "modules/jb2a_patreon/Library/Generic/Explosion/Explosion_04_Dark_Red_400x400.webm";
   }
 
   new Sequence()
-    .sound()
-    .file(
-      "s3/audio/soundboard/Fire%20and%20Explosions/large%20blast/Explosion%20Blast%20Large%2001.ogg"
-    )
-    .effect()
-    .file(effect)
-    .atLocation(template)
-    .center()
-    .scale(1.5)
-    .belowTokens()
-    .play();
+  .sound()
+  .file(
+  "s3/audio/soundboard/Fire%20and%20Explosions/large%20blast/Explosion%20Blast%20Large%2001.ogg"
+  )
+  .effect()
+  .file(effect)
+  .atLocation(template)
+  .center()
+  .scale(1.5)
+  .belowTokens()
+  .play();
 }
 
 async function postEffects(template, token) {
@@ -170,18 +146,17 @@ async function postEffects(template, token) {
 
 const callbacks = {
   pre: async (template, update) => {
-    myEffectFunction(template, update);
-    await warpgate.wait(500);
+  myEffectFunction(template, update);
+  await warpgate.wait(500);
   },
   post: async (template, token) => {
-    postEffects(template, token);
-    await warpgate.wait(500);
-    greetings(template, token);
+  postEffects(template, token);
+  await warpgate.wait(500);
+  greetings(template, token);
   },
 };
 
 const options = { controllingActor: actor };
 
 updates = mergeObject(updates, choice);
-console.log("updates: ", updates);
 warpgate.spawn(summonType, updates, callbacks, options);
--- bigbys_hand/grasping_hand.js
+++ bigbys_hand/grasping_hand.js
@@ -1,135 +1,28 @@
-console.log(args);
-async function wait(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
+let results;
+const attacker = canvas.tokens.get(args[0].tokenId);
+const {object: target} = await fromUuid(args[0].hitTargetUuids[0]);
 
-function filter(array, value, key) {
-  return array.filter(
-    key
-      ? (a) => a[key] === value
-      : (a) => Object.keys(a).some((k) => a[k] === value)
-  );
-}
-const lastArg = args[args.length - 1];
-const actorD = game.actors.get(args[0].actor._id);
-const tokenD = canvas.tokens.get(args[0].tokenId);
-let target = lastArg.targets;
-const ttarget = canvas.tokens.get(lastArg.targets[0].id);
-let uuid = target[0].uuid;
-let targetSize = target[0]._actor.data.data.traits.size;
-let targetCondition = target[0].data.actorData.effects;
-let grappled = filter(targetCondition, "Grappled");
-let actionName = "Grapple";
-let itemDescription =
-  "<p>When you want to grab a creature or wrestle with it, you can use the Attack action to make a Special melee Attack, a grapple. If you’re able to make multiple attacks with the Attack action, this Attack replaces one of them.</p><p>The target of your grapple must be no more than one size larger than you and must be within your reach. Using at least one free hand, you try to seize the target by making a grapple check instead of an Attack roll: a Strength (Athletics) check contested by the target’s Strength (Athletics) or Dexterity (Acrobatics) check (the target chooses the ability to use). If you succeed, you subject the target to the Grappled condition (see Conditions ). The condition specifies the things that end it, and you can release the target whenever you like (no action required).</p>";
-if (targetSize === "grg") {
-  ui.notifications.warn("Target too big to grapple!");
-  return;
-}
-if (grappled[0]?.label === "Grappled") {
-  ui.notifications.info(`${target[0].data.name} already grappled!`);
-  return;
-} else {
-  let bigbySkill = "ath";
-  let targetSkill =
-    target[0].data.document._actor.data.data.skills.ath.total >
-    target[0].data.document._actor.data.data.skills.acr.total
-      ? "ath"
-      : "acr";
-  let bigbyCheck =
-    actorD.data.type === "npc"
-      ? { fastForward: false, chatMessage: false }
-      : { fastForward: true, chatMessage: false };
-  let targetCheck =
-    target[0].data.type === "character"
-      ? { fastForward: false, chatMessage: false }
-      : { fastForward: true, chatMessage: false };
-  let bigbyRoll = await MidiQOL.socket().executeAsGM("rollAbility", {
-    request: "skill",
-    targetUuid: target[0].actor.uuid,
-    ability: bigbySkill,
-    options: bigbyCheck,
-  });
-  let targetRoll = await MidiQOL.socket().executeAsGM("rollAbility", {
-    request: "skill",
-    targetUuid: target[0].actor.uuid,
-    ability: targetSkill,
-    options: targetCheck,
-  });
-  midiExec(actionName, itemDescription, bigbyRoll, targetRoll);
-}
+const skilltoberolled = target.actor.data.data.skills.ath.total < target.actor.data.data.skills.acr.total ? "acr" : "ath";
 
-async function midiExec(actionName, itemDescription, bigbyRoll, targetRoll) {
-  let bigbyRollType = bigbyRoll.terms[0].options.advantage
-    ? " (Advantage)"
-    : bigbyRoll.terms[0].options.disadvantage
-    ? " (Disadvantage)"
-    : "";
-  let targetRollType = targetRoll.terms[0].options.advantage
-    ? " (Advantage)"
-    : targetRoll.terms[0].options.disadvantage
-    ? " (Disadvantage)"
-    : "";
-  game.dice3d?.showForRoll(bigbyRoll);
-  game.dice3d?.showForRoll(targetRoll);
-  let bigbyWin = "";
-  let targetWin = "";
-  bigbyRoll.total >= targetRoll.total
-    ? (bigbyWin = `success`)
-    : (targetWin = `success`);
-  if (bigbyWin === "success") {
-    game.dfreds.effectInterface.addEffect({ effectName: "Grappled", uuid });
+results = await game.MonksTokenBar.requestContestedRoll({
+  token:attacker,
+  request:'skill:ath'
+},{
+  token: target,
+  request: `skill:${skilltoberolled}`
+},{
+  silent:true,
+  fastForward:false,
+  flavor: `${target.name} tries to resist ${attacker.name}'s grapple attempt`,
+  callback: async () => {
+    const attackerTotal = results.getFlag("monks-tokenbar", `token${attacker.id}`).total;
+    const targetTotal = results.getFlag("monks-tokenbar", `token${target.id}`).total;
+    if (attackerTotal >= targetTotal) {
+      if(!game.dfreds.effectInterface.hasEffectApplied('Grappled', target.actor.uuid)) {
+        await game.dfreds.effectInterface.addEffect({ effectName: 'Grappled', uuid: target.actor.uuid});
+        ui.notifications.info(`${attacker.name} grapples ${target.name}!`)
+      }
+    }
+    else ui.notifications.info(`${target.name} resists the grapple from ${attacker.name}`)
   }
-  let damage_results = `
-      <div><h2>${actionName}</h2>${itemDescription}</div>
-    <div class="flexrow 2">
-    <div><div style="text-align:center">${actorD.name}</div></div><div><div style="text-align:center">${target[0].name}</div></div>
-    </div>
-    <div class="flexrow 2">
-      <div>
-        <div style="text-align:center">Acrobatics${bigbyRollType}</div>
-        <div class="dice-roll">
-          <div class="dice-result">
-            <div class="dice-formula">${bigbyRoll.formula}</div>
-            <div class="dice-tooltip">
-              <div class="dice">
-                <header class="part-header flexrow">
-                  <span class="part-formula">${bigbyRoll.formula}</span>
-                  <span class="part-total">${bigbyRoll.total}</span>
-                </header>
-              </div>
-            </div>
-            <h4 class="dice-total ${bigbyWin}">${bigbyRoll.total}</h4>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div style="text-align:center">Acrobatics${targetRollType}</div>
-        <div class="dice-roll">
-          <div class="dice-result">
-            <div class="dice-formula">${targetRoll.formula}</div>
-            <div class="dice-tooltip">
-              <div class="dice">
-                <header class="part-header flexrow">
-                  <span class="part-formula">${targetRoll.formula}</span>
-                  <span class="part-total">${targetRoll.total}</span>
-                </header>
-              </div>
-            </div>
-            <h4 class="dice-total ${targetWin}">${targetRoll.total}</h4>
-          </div>
-        </div>
-      </div>
-    </div>`;
-  const chatMessage = game.messages.get(lastArg.itemCardId);
-  let content = duplicate(chatMessage.data.content);
-  const searchString =
-    /<div class="midi-qol-saves-display">[\s\S]*<div class="end-midi-qol-saves-display">/g;
-  const replaceString = `<div class="midi-qol-saves-display"><div class="end-midi-qol-saves-display">${damage_results}`;
-  content = content.replace(searchString, replaceString);
-  chatMessage.update({ content: content });
-  await wait(500);
-  await ui.chat.scrollBottom();
-}
+});
